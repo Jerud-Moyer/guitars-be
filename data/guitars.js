42910441214affc59e8085aaaa4d77e1e8713736
harriet-the-spy <dpcairns@gmail.com>
2020-08-12 13:51:02 -0700

    initial commit

---
+++ data/guitars.js
@@ -0,0 +1,16 @@
+// SEED data
+
+module.exports = [
+  {
+    color: 'green',
+    strings: 3,
+  },
+  {
+    color: 'red',
+    strings: 4,
+  },
+  {
+    color: 'blue',
+    strings: 10,
+  }
+];
